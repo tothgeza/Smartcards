c6f16980f39a3a11b14020cfa049eef100f19305
Tóth Géza <toth.geza.0425@gmail.com>
2021-09-13 15:52:46 +0200

    Implement Links on NavBar.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,15 +4,24 @@ import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
 import Content from "./components/Content";
 import SideNav from "./components/SideNav";
+import SignUp from "./components/SignUp";
+import SignIn from "./components/SignIn";
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
 
     return (
-        <div className="d-flex flex-column vh-100">
-            <NavBar />
-            <SideNav />
-            <Footer />
-        </div>
+        <Router>
+            <div className="d-flex flex-column vh-100">
+                <NavBar />
+                <Switch>
+                    <Route path="/signin" component={SignIn} />
+                    <Route path="/signup" component={SignUp} />
+                    <Route path="/account" component={SideNav} />
+                </Switch>
+                <Footer />
+            </div>
+        </Router>
     );
 }
 
--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,8 +1,9 @@
-import React from 'react'
+import React from 'react';
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
     return (
-        <nav className="navbar navbar-expand-sm navbar-dark bg-dark py-3" aria-label="Third navbar example">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark py-3" aria-label="Third navbar example">
             <div className="container">
                 <a className="navbar-brand" href="#">SmartCards</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample03" aria-controls="navbarsExample03" aria-expanded="false" aria-label="Toggle navigation">
@@ -11,21 +12,21 @@ const NavBar = () => {
 
                 <div className="collapse navbar-collapse" id="navbarsExample03">
                     <ul className="navbar-nav ms-auto mb-2 mb-sm-0">
-                        <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="#">My Classes</a>
-                        </li>
+                        <Link className="nav-link" to="/account">
+                            My Classes
+                        </Link>
                         <li className="nav-item">
                             <a className="nav-link" href="#">Search Flashcards</a>
                         </li>
                         <li className="nav-item">
                             <a className="nav-link me-4" href="#">Make Flashcards</a>
                         </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">Login</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#" tabindex="-1">Get Started</a>
-                        </li>
+                        <Link className="nav-link " to="/signin">
+                            Login
+                        </Link>
+                        <Link className="nav-link " to="/signup">
+                            Get Started
+                        </Link>
 
                     </ul>
                 </div>
