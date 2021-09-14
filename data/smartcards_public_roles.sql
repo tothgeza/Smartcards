a16da37189f2ec49db2405202dea3764d3f7ee66
Tóth Géza <toth.geza.0425@gmail.com>
2021-09-14 19:28:47 +0200

    Add smartcards_public_roles.sql to upload roles

---
+++ data/smartcards_public_roles.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.roles (id, name) VALUES (1, 'ROLE_USER');
+INSERT INTO public.roles (id, name) VALUES (2, 'ROLE_MODERATOR');
+INSERT INTO public.roles (id, name) VALUES (3, 'ROLE_ADMIN');
--- src/main/java/com/codecool/smartcards/models/Role.java
+++ src/main/java/com/codecool/smartcards/models/Role.java
@@ -3,6 +3,7 @@ package com.codecool.smartcards.models;
 import javax.persistence.*;
 
 @Entity
+@Table(name = "roles")
 public class Role {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
