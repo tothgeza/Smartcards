c2d34c4976cce240989e9b1bbced0319a4fad183
Tóth Géza <toth.geza.0425@gmail.com>
2021-09-15 22:39:05 +0200

    Modify App.js
    Implement new routes.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,36 @@
+import React from 'react';
+import {Route, Switch} from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
 
-import './App.css';
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
-import Content from "./components/Content";
 import SideNav from "./components/SideNav";
-import SignUp from "./components/SignUp";
-import SignIn from "./components/SignIn";
-import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import Profile from "./components/Profile";
+import Register from "./components/Register";
+import Login from "./components/Login";
+import Home from "./components/Home";
+import BoardUser from "./components/BoardUser";
+import BoardModerator from "./components/BoardModerator";
+import BoardAdmin from "./components/BoardAdmin";
 
 function App() {
 
     return (
-        <Router>
-            <div className="d-flex flex-column vh-100">
-                <NavBar />
-                <Switch>
-                    <Route path="/signin" component={SignIn} />
-                    <Route path="/signup" component={SignUp} />
-                    <Route path="/account" component={SideNav} />
-                </Switch>
-                <Footer />
-            </div>
-        </Router>
+        <div className="d-flex flex-column vh-100">
+            <NavBar/>
+            <Switch>
+                <Route exact path={["/", "/home"]} component={Home}/>
+                <Route exact path="/login" component={Login}/>
+                <Route exact path="/register" component={Register}/>
+                <Route exact path="/profile" component={Profile}/>
+                <Route exact path="/myclass" component={SideNav}/>
+                <Route path="/user" component={BoardUser}/>
+                <Route path="/mod" component={BoardModerator}/>
+                <Route path="/admin" component={BoardAdmin}/>
+            </Switch>
+            <Footer/>
+        </div>
     );
 }
 
