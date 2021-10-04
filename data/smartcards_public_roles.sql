5d4d4d85f0ba3233a59e722657a6a8fe3882debd
Tóth Géza <toth.geza.0425@gmail.com>
2021-10-04 14:23:04 +0200

    Remove unused sql file

--- data/smartcards_public_roles.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO public.roles (id, name) VALUES (1, 'ROLE_USER');
-INSERT INTO public.roles (id, name) VALUES (2, 'ROLE_MODERATOR');
-INSERT INTO public.roles (id, name) VALUES (3, 'ROLE_ADMIN');
