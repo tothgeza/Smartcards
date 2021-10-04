ff648177ec628c3da78b5c0b6743a6de9c678607
Tóth Géza <toth.geza.0425@gmail.com>
2021-10-04 14:21:08 +0200

    Implement MyClassRepository interface

--- src/main/java/com/codecool/smartcards/repository/MyClassRepository.java
+++ src/main/java/com/codecool/smartcards/repository/MyClassRepository.java
@@ -1,15 +1,26 @@
 package com.codecool.smartcards.repository;
 
-import com.codecool.smartcards.models.Deck;
+import com.codecool.smartcards.dto.MyClassDTO;
 import com.codecool.smartcards.models.MyClass;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface MyClassRepository extends JpaRepository<MyClass, Long> {
 
-    @Query("select p from MyClass m join m.decks p where m.id = :id")
-    List<Deck> findAllDecks(@Param("id") Long id);
+    @Query("select new com.codecool.smartcards.dto.MyClassDTO(m.id, m.title, m.user.id)" +
+            " from MyClass m where m.user.id = :userID order by m.id asc")
+    List<MyClassDTO> findMyClassByUserIdDTO(@Param("userID") long userID);
+
+    @Query("select m from MyClass m where m.id = :myClassID")
+    Optional<MyClass> findMyClassById(@Param("myClassID") long myClassID);
+
+    @Query("select new com.codecool.smartcards.dto.MyClassDTO(m.id, m.title, m.user.id)" +
+            " from MyClass m where m.id = :myClassID")
+    Optional<MyClassDTO> findMyClassByIdDTO(@Param("myClassID") long myClassID);
+
+
 }
