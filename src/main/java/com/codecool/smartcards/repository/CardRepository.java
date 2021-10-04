9ce64ed1e6ed7512d6c886a2fdee752206fb9b56
Tóth Géza <toth.geza.0425@gmail.com>
2021-10-04 14:21:53 +0200

    Implement CardRepository interface

--- src/main/java/com/codecool/smartcards/repository/CardRepository.java
+++ src/main/java/com/codecool/smartcards/repository/CardRepository.java
@@ -1,7 +1,26 @@
 package com.codecool.smartcards.repository;
 
+import com.codecool.smartcards.dto.CardDTO;
 import com.codecool.smartcards.models.Card;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface CardRepository extends JpaRepository<Card, Long> {
+
+    @Query("select new com.codecool.smartcards.dto.CardDTO(" +
+            "c.id, c.question, c.answer, c.deck.id, c.deck.myClass.id, c.deck.myClass.user.id)" +
+            " from Card c where c.deck.id = :deckID order by c.id asc")
+    List<CardDTO> findCardByDeckIdDTO(@Param("deckID") long deckID);
+
+    @Query("select c from Card c where c.id = :cardID")
+    Optional<Card> findCardById(@Param("cardID") long cardID);
+
+    @Query("select new com.codecool.smartcards.dto.CardDTO(" +
+            "c.id, c.question, c.answer, c.deck.id, c.deck.myClass.id, c.deck.myClass.user.id)" +
+            " from Card c where c.id = :cardID")
+    Optional<CardDTO> findCardByIdDTO(@Param("cardID") long cardID);
 }
