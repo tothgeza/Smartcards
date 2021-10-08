d285cf56ac7b6686bdfdc08816a6488b6ebae655
Tóth Géza <toth.geza.0425@gmail.com>
2021-10-08 18:27:07 +0200

    Implement change folder icon when click

--- frontend/src/components/MyClasses/SideNav.js
+++ frontend/src/components/MyClasses/SideNav.js
@@ -2,13 +2,15 @@ import React, {useEffect, useRef, useState} from 'react';
 import {IoAddOutline, IoSearchOutline, IoFolderOpen} from 'react-icons/io5';
 import MyClassService from "../../services/myClass.service";
 import Modals from "./Modals/modals";
+import {FcFolder, FcOpenedFolder} from 'react-icons/fc';
 import {Link} from "react-router-dom";
 
 const SideNav = ({activeMyClass, setActiveMyClass, setIsActiveMyClass}) => {
 
-  const refMyClassLink  = useRef([]);
+  const refMyClassLink = useRef([]);
   const [myClasses, setMyClasses] = useState([]);
   const [showCreateMyClassModal, setShowCreateMyClassModal] = useState(false);
+  const [activeMyClassIndex, setActiveMyClassIndex] = useState(-1);
 
   const openCreateMyClassModal = (event) => {
     event.preventDefault();
@@ -17,10 +19,16 @@ const SideNav = ({activeMyClass, setActiveMyClass, setIsActiveMyClass}) => {
 
   const handleClickMyClass = (event, myClass, index) => {
     event.preventDefault();
+    if (activeMyClassIndex > -1) {
+      const previousMyClass = refMyClassLink.current[activeMyClassIndex];
+      previousMyClass.classList.remove("link-myClass-active")
+      previousMyClass.classList.add("link-myClass")
+      setActiveMyClassIndex(-1);
+    }
     const currentMyClass = refMyClassLink.current[index];
-    currentMyClass.classList.remove("link-class")
-    currentMyClass.classList.add("link-class-active")
-    console.log(currentMyClass);
+    currentMyClass.classList.remove("link-myClass")
+    currentMyClass.classList.add("link-myClass-active")
+    setActiveMyClassIndex(index);
     setActiveMyClass(myClass)
     setIsActiveMyClass(true);
   }
@@ -52,22 +60,23 @@ const SideNav = ({activeMyClass, setActiveMyClass, setIsActiveMyClass}) => {
 
   return (
     <div className="flex-shrink-0 side-nav">
-      <div className="d-flex align-items-center pb-3 mb-3 border-bottom justify-content-between">
+      <div className="d-flex align-items-center mt-3 mb-2 pb-3">
         <div className="flex-grow-1 align-items-center">
-          <p className={"ms-3 mb-0 fs-6 "}>MY CLASSES ({myClasses.length})</p>
+          <p className={"ms-3 my-auto fs-6"} style={{color: "white"}}>MY CLASSES ({myClasses.length})</p>
+        </div>
+        <div className="sideNav-divider">
         </div>
         <div className="px-2">
           {/*Create New MyClass Link*/}
           <Link to="#0"
-                className="link-class"
-                style={{textDecoration: 'none'}}
+                className="link-sidenav"
                 onClick={(event) => openCreateMyClassModal(event)}>
             <IoAddOutline size={"2em"}/>
           </Link>
         </div>
         <div className="px-2">
           {/*Search Decks Link*/}
-          <Link to="#0" className="class-link " style={{textDecoration: 'none'}}>
+          <Link to="#0" className="link-sidenav me-2" style={{textDecoration: 'none'}}>
             <IoSearchOutline size={"2em"}/>
           </Link>
         </div>
@@ -76,38 +85,44 @@ const SideNav = ({activeMyClass, setActiveMyClass, setIsActiveMyClass}) => {
       <div className="container-fluid">
         {/* List MyClasses*/}
         {myClasses.map((myClass, index) => (
-          <Link to={"#0"} className="row px-0 py-2 mt-3 link-class"
+          <Link to={"#0"} className="row ps-3 pe-0 link-myClass"
                 style={{textDecoration: 'none'}}
                 ref={(element) => refMyClassLink.current.push(element)}
                 id={myClass.id}
                 key={myClass.id}
                 onClick={(event) => handleClickMyClass(event, myClass, index)}>
-              <div className="col-1 ms-3"><IoFolderOpen size={"2em"}/></div>
-              <div className="col ms-3 my-auto">
-                <p className={"mb-0"} style={{fontSize: "14px", fontWeight: "500"}}>
-                  {myClass.title}
-                </p>
-              </div>
+            <div className="col-1 ms-3 class-icon">
+              {activeMyClass.id === myClass.id ? (
+                <FcOpenedFolder size={"2em"}/>
+              ) : (
+                <FcFolder size={"2em"}/>
+              )}
+            </div>
+            <div className="col ms-3 my-auto">
+              <p className={"mb-0"} style={{fontSize: "14px", fontWeight: "500"}}>
+                {myClass.title}
+              </p>
+            </div>
           </Link>
         ))}
-        <div className={"mt-4"}>
-          {/*Create New MyClass Link*/}
-          <a href=""
-             className="row align-items-center ms-2 mb-3 link-sidenav"
-             style={{textDecoration: 'none'}}
-             onClick={(event) => openCreateMyClassModal(event)}>
-            <div className="col-1"><IoAddOutline size={"1.5em"}/></div>
-            <div className="col ms-3" style={{fontSize: "12px", fontWeight: "500"}}>Create New Class
-            </div>
-          </a>
-          {/*Search Decks Link*/}
-          <a href="" className="row align-items-center ms-2 mb-3 link-sidenav"
-             style={{textDecoration: 'none'}}>
-            <div className="col-1"><IoSearchOutline size={"1.5em"}/></div>
-            <div className="col ms-3" style={{fontSize: "12px", fontWeight: "500"}}>Find Flashcards
-            </div>
-          </a>
-        </div>
+        {/*Create New MyClass Link*/}
+        <Link to={"#0"} className={"row ps-3 pe-0 mt-3 link-sidenav"}
+              onClick={(event) => openCreateMyClassModal(event)}>
+          <div className="col-1 ms-3">
+            <IoAddOutline size={"1.5em"} style={{position: "relative", top: "-4px", left: "3px"}}/>
+          </div>
+          <p className="col ms-3" style={{fontSize: "12px", fontWeight: "500"}}>Create New Class
+          </p>
+        </Link>
+        {/*Search Decks Link*/}
+        <Link to={"#0"} className={"row ps-3 pe-0 mt-2 link-sidenav"}>
+          <div className="col-1 ms-3">
+            <IoSearchOutline size={"1.5em"} style={{position: "relative", top: "-4px", left: "3px"}}/>
+          </div>
+          <p className="col ms-3" style={{fontSize: "12px", fontWeight: "500"}}>Create New Class
+          </p>
+        </Link>
+
       </div>
 
       {/* Create new myclass modal */}
