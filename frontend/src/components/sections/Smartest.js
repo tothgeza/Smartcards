850be8d49ee6df598b6c34106633e39927f49626
Tóth Géza <toth.geza.0425@gmail.com>
2021-10-10 19:10:01 +0200

    Implement Smartest.js
    About section

---
+++ frontend/src/components/sections/Smartest.js
@@ -0,0 +1,105 @@
+import React from "react";
+import "./Smartest.css";
+
+function Services() {
+  return (
+    <>
+      <section id="smartest" className="services section-bg">
+        <div className="container">
+          <header className="section-header text-center mb-5">
+            <h2>The world's smartest flashcard app.</h2>
+          </header>
+          <div className="row">
+            <div className="col-md-6 col-lg-4 wow bounceInUp">
+              <div className="box">
+                <div className="icon">
+                  <i className="ion-ios-analytics-outline"></i>
+                </div>
+                <h4 className="title">
+                  <a href="#">Lorem Ipsum</a>
+                </h4>
+                <p className="description">
+                  Voluptatum deleniti atque corrupti quos dolores et quas
+                  molestias excepturi sint occaecati cupiditate non provident
+                </p>
+              </div>
+            </div>
+            <div className="col-md-6 col-lg-4">
+              <div className="box">
+                <div className="icon">
+                  <i className="ion-ios-bookmarks-outline"></i>
+                </div>
+                <h4 className="title">
+                  <a href="#">Dolor Sitema</a>
+                </h4>
+                <p className="description">
+                  Minim veniam, quis nostrud exercitation ullamco laboris nisi
+                  ut aliquip ex ea commodo consequat tarad limino ata
+                </p>
+              </div>
+            </div>
+
+            <div className="col-md-6 col-lg-4">
+              <div className="box">
+                <div className="icon">
+                  <i className="ion-ios-paper-outline"></i>
+                </div>
+                <h4 className="title">
+                  <a href="#">Sed ut perspiciatis</a>
+                </h4>
+                <p className="description">
+                  Duis aute irure dolor in reprehenderit in voluptate velit esse
+                  cillum dolore eu fugiat nulla pariatur
+                </p>
+              </div>
+            </div>
+            <div className="col-md-6 col-lg-4 wow">
+              <div className="box">
+                <div className="icon">
+                  <i className="ion-ios-speedometer-outline"></i>
+                </div>
+                <h4 className="title">
+                  <a href="#">Magni Dolores</a>
+                </h4>
+                <p className="description">
+                  Excepteur sint occaecat cupidatat non proident, sunt in culpa
+                  qui officia deserunt mollit anim id est laborum
+                </p>
+              </div>
+            </div>
+
+            <div className="col-md-6 col-lg-4">
+              <div className=" box">
+                <div className="icon">
+                  <i className="ion-ios-world-outline"></i>
+                </div>
+                <h4 className="title">
+                  <a href="#">Nemo Enim</a>
+                </h4>
+                <p className="description">
+                  At vero eos et accusamus et iusto odio dignissimos ducimus qui
+                  blanditiis praesentium voluptatum deleniti atque
+                </p>
+              </div>
+            </div>
+            <div className="col-md-6 col-lg-4">
+              <div className="box">
+                <div className="icon">
+                  <i className="ion-ios-clock-outline"></i>
+                </div>
+                <h4 className="title">
+                  <a href="#">Eiusmod Tempor</a>
+                </h4>
+                <p className="description">
+                  Et harum quidem rerum facilis est et expedita distinctio. Nam
+                  libero tempore, cum soluta nobis est eligendi
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
+export default Services;
