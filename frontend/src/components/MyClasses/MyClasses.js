c21dfa301e6ba164862bd38cf7639eb9cfac2f8a
Tóth Géza <toth.geza.0425@gmail.com>
2021-10-15 04:16:02 +0200

    Implement responsive sidenav

--- frontend/src/components/MyClasses/MyClasses.js
+++ frontend/src/components/MyClasses/MyClasses.js
@@ -5,6 +5,10 @@ import Content from "./Content";
 import ContentHeader from "./ContentHeader";
 import MyClassService from "../../services/myClass.service";
 import './myclasses.css';
+import {Offcanvas} from "react-bootstrap";
+import {AiOutlineMenuUnfold} from "react-icons/ai";
+import {Link} from "react-router-dom";
+import {FcFolder} from "react-icons/fc";
 
 const MyClasses = () => {
 
@@ -16,6 +20,13 @@ const MyClasses = () => {
 
   const [myClasses, setMyClasses] = useState([]);
   const [isActiveMyClass, setIsActiveMyClass] = useState(false);
+
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = (event) => {
+    event.preventDefault();
+    setShow(true);
+  }
 
   const fetchMyClass = () => {
     MyClassService.getMyClasses()
@@ -40,14 +51,37 @@ const MyClasses = () => {
     <div>
       {isUserLoggedIn ? (
         <main>
-          <SideNav
-            activeMyClass={activeMyClass}
-            setActiveMyClass={setActiveMyClass}
-            setIsActiveMyClass={setIsActiveMyClass}
-            fetchMyClass={fetchMyClass}
-            myClasses={myClasses}
-          />
-          {/* Vertical Divider */}
+          <div className={"side-nav-link"}>
+            <Link to={"/#0"}
+                  onClick={(event) => handleShow(event)}>
+              <FcFolder size={"2em"} />
+            </Link>
+          </div>
+          <Offcanvas show={show} onHide={handleClose}>
+            <Offcanvas.Header closeButton
+                              style={{backgroundColor:" #ECF6FF"}}
+            >
+            </Offcanvas.Header>
+            <Offcanvas.Body className={"p-0"} style={{width:"auto"}} scroll={false}>
+              <SideNav
+                activeMyClass={activeMyClass}
+                setActiveMyClass={setActiveMyClass}
+                setIsActiveMyClass={setIsActiveMyClass}
+                fetchMyClass={fetchMyClass}
+                myClasses={myClasses}
+              />
+            </Offcanvas.Body>
+          </Offcanvas>
+          <div className={"flex-shrink-0 h-100 side-nav-wrapper"}>
+            <SideNav
+              activeMyClass={activeMyClass}
+              setActiveMyClass={setActiveMyClass}
+              setIsActiveMyClass={setIsActiveMyClass}
+              fetchMyClass={fetchMyClass}
+              myClasses={myClasses}
+            />
+          </div>
+           {/*Vertical Divider */}
           {/*<div className="b-example-divider"/>*/}
           {
             isActiveMyClass &&
